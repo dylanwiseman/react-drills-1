5aa0a7dfd2f328d5cc1addcbe1cb5452e70ad9ec
Dylan Wiseman <dylan.wiseman@getweave.com>
2022-01-14 08:59:05 -0700

    app-8

--- app-8/src/App.js
+++ app-8/src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    const [character, setCharacter] = useState({});
+
+    const getData = async () => {
+        const { data } = await axios.get('https://swapi.dev/api/people/2');
+        setCharacter(data);
+    };
+
+    useEffect(() => {
+        getData();
+    }, []);
+    return (
+        <div>
+            <h1>Star Wars API</h1>
+            <h2>Name: {character?.name}</h2>
+            <h2>Birth Year: {character?.birth_year}</h2>
+            <h2>Height: {character?.height}</h2>
+            <h2>Eye Color: {character?.eye_color}</h2>
+        </div>
+    );
 }
-
-export default App;
